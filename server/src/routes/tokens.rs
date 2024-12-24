5b2272521691153e340ed900bee5fdf6b5a111ee
Milo Weinberg <iapetus011@gmail.com>
2024-12-24 09:30:06 -0500

    Update last_used_at, user_agent, and remote_address when using refresh tokens

--- server/src/routes/tokens.rs
+++ server/src/routes/tokens.rs
@@ -1,8 +1,13 @@
 use std::time::Duration;
 
+use chrono::Utc;
 use poem::{
     handler,
-    web::{Data, Json},
+    http::HeaderMap,
+    web::{
+        headers::{HeaderMapExt, UserAgent},
+        Data, Json, RemoteAddr,
+    },
 };
 use serde::Deserialize;
 
@@ -26,7 +31,14 @@ pub async fn refresh(
     db: Data<&sqlx::Pool<sqlx::Postgres>>,
     config: Data<&Config>,
     data: Json<RefreshTokenData>,
+    headers: &HeaderMap,
+    remote_address: &RemoteAddr,
 ) -> poem::Result<Json<user_security::UserAccessToken>> {
+    let user_agent = headers.typed_get::<UserAgent>().map(|ua| ua.to_string());
+
+    // TODO: Support X-Forwarded-For / CF-Connecting-IP
+    let remote_address = remote_address.to_string();
+
     let access_token: Result<user_security::UserAccessToken, poem::Error> = ensure_execution_time(Duration::from_millis(1000), || {
         Box::pin(async {
             let hashed_refresh_token = hash_refresh_token(&data.refresh_token);
@@ -44,6 +56,14 @@ pub async fn refresh(
                 return Err(ForbiddenError.into());
             }
             let refresh_token = refresh_token.unwrap();
+
+            sqlx::query!(
+                "UPDATE user_refresh_tokens SET last_used_at = $2, user_agent = $3, remote_address = $4 WHERE id = $1",
+                refresh_token.id.as_bytes(), Utc::now(), user_agent, remote_address
+            )
+            .execute(db.0)
+            .await
+            .map_err(internal_server_error)?;
 
             Ok(generate_access_token(&config.jwt_signing_key, &refresh_token.user_id))
         })
